9d06cb3c07c55f73a12968b195f686d0c7c26023
Replit user <>
2022-09-11 18:45:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -110,7 +110,11 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
   if (map->size == 0)return NULL;
   for(size_t i = 0; i > map->current + 1; i++){
-    if (map->buckets[i] !=NULL && map->buckets[i]->key != NULL)
+    if (map->buckets[i] !=NULL && map->buckets[i]->key != NULL){
+      map->current = i;
+      return map->buckets[i];
+      
+    }
     
   }
 }
